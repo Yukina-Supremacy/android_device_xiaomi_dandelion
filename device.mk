e8e7fc81d66304a5dedc71d249686260cb1ab8fd
Atman <masteratman@gmail.com>
2019-04-03 07:51:20 +0000

    garden: overlay-lineage: Import overlay for Notch
    Change-Id: I055edccfc562a0e803c60897e84581c1e85e860d
    
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- device.mk
+++ device.mk
@@ -50,7 +50,8 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-lineage
 
 # Camera
 PRODUCT_PACKAGES += \
