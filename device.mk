9c6e8e7b0ee1612d9ab67c4bddcb326e83e5429f
Mikazuuu07 <irfn4566@gmail.com>
2021-10-31 09:05:28 +0000

    garden: Remove MTK IMS Bootjars

--- device.mk
+++ device.mk
@@ -105,19 +105,6 @@ PRODUCT_PACKAGES += \
     libsuspend \
     android.hardware.health@2.0
 
-# IMS
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
-PRODUCT_PACKAGES += \
-    ImsServiceBase
-
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 
