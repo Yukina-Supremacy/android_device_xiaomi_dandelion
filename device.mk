92c26d78ebd0832b35fc54a0a06e0f1789e776aa
Mikazuuu07 <irfn4566@gmail.com>
2022-01-01 13:08:29 +0700

    garden: Enable MTK IMS Bootjars
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- device.mk
+++ device.mk
@@ -105,6 +105,19 @@ PRODUCT_PACKAGES += \
     libsuspend \
     android.hardware.health@2.0
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
+PRODUCT_PACKAGES += \
+    ImsServiceBase
+
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 
