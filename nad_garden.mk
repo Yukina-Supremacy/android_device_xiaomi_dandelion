cefc36a8fb940ab475d99a093209177c752fbc49
Mikazuuu07 <mikazuuu07@xzi-os.com>
2022-04-08 09:01:00 +0700

    garden: Update Build Fingerprint Raven April'22
    Signed-off-by: Mikazuuu07 <mikazuuu07@xzi-os.com>

--- nad_garden.mk
+++ nad_garden.mk
@@ -42,12 +42,12 @@ PRODUCT_MANUFACTURER := xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := garden
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 12 SP2A.220305.012 8177914 release-keys"
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220405.004 8233519 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:12/SP2A.220305.012/8177914:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220405.004/8233519:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
